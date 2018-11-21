ad2fb4a1270cda70dec6fb0d84c5e2d9270d5032
Gonzalo <gonzalo224455@gmail.com>
2018-11-21 16:36:42 +0100

    Primer Commit

---
+++ FXSlide/src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+	    FXMLLoader loader =new FXMLLoader(getClass().getResource("ventana.fxml"));
+        AnchorPane pane=loader.load();
+        Scene scene=new Scene(pane);
+        primaryStage.setTitle("Slide");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
